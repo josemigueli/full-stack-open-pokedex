df29129278e9ecf0a4f92e97cebfb5325537755a
Miguel Osorio <miguelusx@gmail.com>
2025-05-04 20:32:02 -0600

    V.8

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('7.0')
+  res.send('8.0')
 })
 
 app.get('/health', (_req, res) => {
