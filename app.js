a942bbcfdef7db94fd5254807ed1a026be06437b
Miguel Osorio <miguelusx@gmail.com>
2025-05-04 19:02:27 -0600

    V.5

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('4.0')
+  res.send('5.0')
 })
 
 app.get('/health', (_req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
+  // if (true) throw('error...  ')
   res.send('ok')
 })
 
