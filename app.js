d5f3804b2838d0a1332b2b64bdefb8a4daf19831
Miguel Osorio <miguelusx@gmail.com>
2025-05-10 15:02:56 -0600

    Exercise 11.18

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('11.1.0')
+  res.send('11.2.0')
 })
 
 app.get('/health', (_req, res) => {
